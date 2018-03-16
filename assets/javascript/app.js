39d15b32f1ca86fc6394e943d22a7773ad8082ff
Joellen Terranova <joellen.leigh@gmail.com>
2018-03-16 12:51:22 -0400

    added all trivia questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+// This array containts all of our questions and answer options, as well as correct answers.
+questions = [
+    {
+        q: "Who is the apprentice of Medivh the prophet?",
+        ao: ["Tyrande","Khadgar","Arthas Menethil","Alexstrasza"],
+        a: "Khadgar",
+        image: "assets/images/khadgar.jpg"
+        // Khadgar is now one of the most powerful mages to have ever lived, and got his beginnings by being an apprentice to Medivh
+    },
+    {
+        q: "Who is the dragon aspect of time?",
+        ao: ["Nozdormu","Ysera","Chromie","Thrall"],
+        a: "Nozdormu",
+        image: "assets/images/nozdormu.jpg"
+        // Nozdormu is the bronze dragon aspect, and protector of the sands of time.
+    },
+    {
+        q: "What is the level cap in the Wrath of the Lich King expansion?",
+        ao: ["75","60","80","55"],
+        a: "80",
+        image: "assets/images/wrath.jpg"
+        // Level cap during Wrath was 80. The game now levels to 110 from the Legion expansion.
+    },
+    {
+        q: "Jaina Proudmoore is the founder and former lady of what region in Kalimdor?",
+        ao: ["Orgrimmar","Uldum","Theramore","Silithus"],
+        a: "Theramore",
+        image: "assets/images/jaina.jpg"
+        // Jaina is the founder of Theramore Isle
+    },
+    {
+        q: "What is the name of the Lich King's sword?",
+        ao: ["Frostmourne","Glamdring","Taeshalach","Light's Vengeance"],
+        a: "Frostmourne",
+        image: "assets/images/frostmourne.jpg"
+        // Arthas' famous sword is known as Frostmourne
+    },
+    {
+        q: "Azeroth is NOT the native home for which horde race?",
+        ao: ["Orc","Tauren","Goblins", "Draenei"],
+        a: "Orc",
+        image: "assets/images/orc.jpg"
+        // Orc's entered Azeroth from Draenor through the dark portal
+    },
+    {
+        q: "Who is a prominent Night Elf leader and high priestess of Elune?",
+        ao: ["Tyrande Whisperwind","Illidan Stormrage","Queen Azshara","Jarod Shadowsong"],
+        a: "Tyrande Whisperwind",
+        image: "assets/images/tyrande.jpg"
+        // Tyrande is the chosen high priestess of the godess Elune and a prominent leader of the Night Elf race.
+    },
+    {
+        q: "Which of these capital cities is home to the Alliance?",
+        ao: ["Silvermoon City","Thunder Bluff","Stormwind City","Garadar"],
+        a: "Stormwind City",
+        image: "assets/images/stormwind.png"
+        // Stormwind City is a capital city home to the Alliance, located in Eastern Kingdoms.
+    },
+    {
+        q: "Deathwing the Destroyer, formerly known as Neltharion the Earth-Warder, was the leader of which dragonflight?",
+        ao: ["Black","Bronze","Red","Fire"],
+        a: "Black",
+        image: "assets/images/deathwing.jpg"
+        // Deathwing was the leader of the black dragonflight before turning against the other dragon aspects during the War of the Ancients.
+    },
+]
